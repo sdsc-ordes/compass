116eec9a8ed1f2a687c8a26c369b1632a5152146
Laure Vancau <laure.vancauwenberghe@epfl.ch>
2026-09-02 11:00:11 +0200

    fix(nix): mac OS compatibility

--- tools/nix/flake.nix
+++ tools/nix/flake.nix
@@ -7,21 +7,25 @@
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        inherit system;
+        pkgs = nixpkgs.legacyPackages.${system};
+      });
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.just
-          pkgs.uv
-          pkgs.nodejs_22
-          pkgs.python311
-          pkgs.stdenv.cc.cc.lib
-        ];
-        shellHook = ''
-          export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
-        '';
-      };
+      devShells = forEachSystem ({ pkgs, ... }: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.just
+            pkgs.uv
+            pkgs.nodejs_22
+            pkgs.python311
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ pkgs.stdenv.cc.cc.lib ];
+          shellHook = pkgs.lib.optionalString pkgs.stdenv.isLinux ''
+            export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+          '';
+        };
+      });
     };
 }
